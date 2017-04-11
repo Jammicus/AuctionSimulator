80d2865eec0ba90f50285bfefa868c3f4aa638ef
Jammicus <jamesmaxwalter@gmail.com>
2017-04-11 20:38:25 +0100

    More tests

--- src/Auction.java
+++ src/Auction.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public interface Auction{
 
-     Bidder simulateAuction(List<Bidder> bidders);
+     Bidder simulateAuction(List<Bidder> bidders, double auctionValue);
 
 
 }
--- test/AuctionTests.java
+++ test/AuctionTests.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AuctionTests {
 
-
+    //refactor
     @Test
     public void returnHighestBidder(){
         Auction fp = new FirstPrice();
@@ -21,7 +21,42 @@ public class AuctionTests {
         listOfBidders.add(bidderPrimePrime);
 
 
-        assertEquals(bidderPrime.getMoney(), (fp.simulateAuction(listOfBidders)).getMoney());
+        assertEquals(bidderPrime.getMoney(), (fp.simulateAuction(listOfBidders,250)).getMoney());
+
+    }
+
+    //First Price Tests
+
+    @Test
+    public void simulateFPAuctionTieBreak() {
+        Auction fp = new FirstPrice();
+        Bidder bidder = new Bidder(1,123);
+        Bidder bidderPrime = new Bidder(2,123);
+        Bidder bidderPrimePrime = new Bidder(3,120);
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+
+        assertTrue(fp.simulateAuction(listOfBidders,100).equals(bidder) ||fp.simulateAuction(listOfBidders,100).equals(bidderPrime));
+    }
+    @Test
+    public void simulateFPAuctionNoTieBreak() {
+        Auction fp = new FirstPrice();
+        Bidder bidder = new Bidder(1,150);
+        Bidder bidderPrime = new Bidder(2,125);
+        Bidder bidderPrimePrime = new Bidder(3,100);
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+
+        assertTrue(fp.simulateAuction(listOfBidders,100).equals(bidder));
+
+    }
+
+    @Test
+    public void bidderWithMoneyLessThanAuctionValueDoesNotWin(){
 
     }
 }
