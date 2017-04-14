5a2429dce7f08d930ff653a86966f72159d63aac
Jammicus <jamesmaxwalter@gmail.com>
2017-04-14 17:43:16 +0100

    Failing Descending tests added

--- test/DescendingTests.java
+++ test/DescendingTests.java
@@ -1,5 +1,87 @@
-/**
- * Created by james_walter on 14/04/2017.
- */
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class DescendingTests {
+
+    @Test
+    public void simulateAuctionTieBreakSecondPrice() {
+        Auction ds = new Descending();
+        Bidder bidder = new Bidder(1, 200);
+        Bidder bidderPrime = new Bidder(2, 200);
+        Bidder bidderPrimePrime = new Bidder(3, 100);
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+
+        assertTrue((ds.simulateAuction(listOfBidders, 900).getWinningBidderID()) == bidder.getId() ||
+                (ds.simulateAuction(listOfBidders, 900).getWinningBidderID()) == bidderPrime.getId());
+    }
+
+
+    @Test
+    public void simulateAuctionNoTieBreakSecondPrice() {
+        Auction ds = new Descending();
+        Bidder bidder = new Bidder(1, 200);
+        Bidder bidderPrime = new Bidder(2, 150);
+        Bidder bidderPrimePrime = new Bidder(3, 100);
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+
+        assertTrue(ds.simulateAuction(listOfBidders, 900).getWinningBidderID() == bidder.getId());
+    }
+
+    @Test
+    public void bidderWithMoneyLessThanAuctionValueDoesNotWinSecondPrice() {
+        Auction ds = new Descending();
+        Bidder bidder = new Bidder(1, 200);
+        Bidder bidderPrime = new Bidder(2, 100);
+
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+
+        assertFalse(ds.simulateAuction(listOfBidders, 900).getWinningBidderID() == bidderPrime.getId());
+    }
+
+    @Test
+    public void tieBreakSecondPrice() {
+        Auction ds = new Descending();
+        Bidder bidder = new Bidder(1, 200);
+        Bidder bidderPrime = new Bidder(2, 200);
+        Bidder bidderPrimePrime = new Bidder(3, 100);
+        Bidder bidderPrimePrimePrime = new Bidder(4, 125);
+
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+        listOfBidders.add(bidderPrimePrimePrime);
+
+        assertTrue((ds.simulateAuction(listOfBidders, 900).getWinningBidderID() == bidder.getId())
+                || ds.simulateAuction(listOfBidders, 900).getWinningBidderID() == bidderPrime.getId());
+    }
+
+    @Test
+    public void correctPriceWinningBidderPaysSecondPrice() {
+        Auction ds = new Descending();
+        Bidder bidder = new Bidder(1, 200);
+        Bidder bidderPrime = new Bidder(2, 125);
+        Bidder bidderPrimePrime = new Bidder(3, 100);
+
+        List<Bidder> listOfBidders = new ArrayList<Bidder>();
+        listOfBidders.add(bidder);
+        listOfBidders.add(bidderPrime);
+        listOfBidders.add(bidderPrimePrime);
+
+        assertTrue(ds.simulateAuction(listOfBidders, 900).getPriceWinningBidderPays() == bidderPrime.getMoney());
+    }
 }
